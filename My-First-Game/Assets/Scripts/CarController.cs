e38975bee9ad9ef17dc35141cf3db56294d8dc2b
Empsu <empsuhakala@gmail.com>
2026-02-06 15:02:53 +0200

    game update
    Added some fuel pickups

--- My-First-Game/Assets/Scripts/CarController.cs
+++ My-First-Game/Assets/Scripts/CarController.cs
@@ -5,6 +5,8 @@ public class CarController : MonoBehaviour
     //Gets Horizontal and Vertical Input
     private float horizontalInput;
     private float verticalInput;
+
+    public float fuel = 10f;
     //Sets the Speed and TurnSpeed
     public float speed = 10f;
     public float turnSpeed = 30f;
@@ -17,6 +19,13 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (fuel <= Time.deltaTime)
+        {
+            Destroy(gameObject);
+        }
+
+        fuel -= Time.deltaTime;
+        // Debug.Log("Fuel: " + fuel);
         //Turns the inputs in to corresponding axis
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -24,5 +33,17 @@ public class CarController : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         //Makes the car turn
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Checks for gameObject with name "Car"
+        if (collision.gameObject.name == "FuelPickUp")
+        {
+            Debug.Log("Meow");
+            //If gameObject name matches destroy
+            fuel += 5;
+        }
+
     }
 }
